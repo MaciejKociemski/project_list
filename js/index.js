a70b34aede388f3bb973467427bf8ccd09e799b2
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-04-29 23:45:31 +0200

    fix and feat and style footer and social footer

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-const slider = document.querySelector('.slider');
-const images = document.querySelectorAll('.slider img');
+// const slider = document.querySelector('.slider');
+// const images = document.querySelectorAll('.slider img');
 
-let currentSlide = 0;
-const slideInterval = setInterval(nextSlide, 500);
+// let currentSlide = 0;
+// const slideInterval = setInterval(nextSlide, 500);
 
-function nextSlide() {
-  images[currentSlide].classList.remove('active');
-  currentSlide = (currentSlide + 1) % images.length;
-  images[currentSlide].classList.add('active');
-}
+// function nextSlide() {
+//   images[currentSlide].classList.remove('active');
+//   currentSlide = (currentSlide + 1) % images.length;
+//   images[currentSlide].classList.add('active');
+// }
