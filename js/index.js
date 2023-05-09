f5421c3bbd50eba986a4abc0b90f5c5dd32db6f6
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-05-09 19:03:40 +0200

    feat add link id

--- js/index.js
+++ js/index.js
@@ -1,14 +1,4 @@
-// const slider = document.querySelector('.slider');
-// const images = document.querySelectorAll('.slider img');
 
-// let currentSlide = 0;
-// const slideInterval = setInterval(nextSlide, 500);
-
-// function nextSlide() {
-//   images[currentSlide].classList.remove('active');
-//   currentSlide = (currentSlide + 1) % images.length;
-//   images[currentSlide].classList.add('active');
-// }
 const slides = document.querySelector('.slides');
 const movingText = document.querySelector('.moving-text');
 
